7cfe5376443d80e9a1bf6356a5201f9121d1d7e8
xuguodong1999 <171860633@smail.nju.edu.cn>
2023-01-08 12:08:40 +0800

    coordgenlibs: use cmake generated export config

--- include/coordgenlibs/CoordgenConfig.hpp
+++ include/coordgenlibs/CoordgenConfig.hpp
@@ -1,22 +1,7 @@
 #pragma once
 
-#ifndef STATIC_COORDGEN
+#include <coordgenlibs_export.hpp>
 
-#ifdef WIN32
-#ifdef IN_COORDGEN
-#define EXPORT_COORDGEN __declspec(dllexport)
-#else
-#define EXPORT_COORDGEN __declspec(dllimport)
-#endif // IN_COORDGEN
-
-#else
-
-#define EXPORT_COORDGEN __attribute__((visibility("default")))
-
-#endif // WIN32
-
-#else
-
-#define EXPORT_COORDGEN
-
-#endif // STATIC_COORDGEN
+#ifndef EXPORT_COORDGEN
+#define EXPORT_COORDGEN COORDGENLIBS_EXPORT
+#endif
