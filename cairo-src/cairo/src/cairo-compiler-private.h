d9a8eb2bb8e82d1d5fd6e094354068c7999a5339
xuguodong1999 <171860633@smail.nju.edu.cn>
2023-08-02 14:16:20 +0800

    fix(cairo): remove slim symbol hidden macros

--- cairo-src/cairo/src/cairo-compiler-private.h
+++ cairo-src/cairo/src/cairo-compiler-private.h
@@ -84,7 +84,7 @@
  * don't need to be hidden and re-exported using the slim hidden
  * macros.
  */
-#if __GNUC__ >= 3 && defined(__ELF__) && !defined(__sun) && !defined(__clang__)
+#if 0 && __GNUC__ >= 3 && defined(__ELF__) && !defined(__sun) && !defined(__clang__)
 # define slim_hidden_proto(name)		slim_hidden_proto1(name, slim_hidden_int_name(name)) cairo_private
 # define slim_hidden_proto_no_warn(name)	slim_hidden_proto1(name, slim_hidden_int_name(name)) cairo_private_no_warn
 # define slim_hidden_def(name)			slim_hidden_def1(name, slim_hidden_int_name(name))
